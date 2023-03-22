50eabee1995e4be54e9de52bc30dc0ef534eec88
Yuri Desideri <yuridesideri12@gmail.com>
2023-03-21 21:42:31 -0300

    feat: loaded arcade model

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,55 @@
 import "./reset.css";
 import { createWorld } from "./world";
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const app = document.querySelector<HTMLDivElement>("#app");
 
-const p = document.createElement("p");
-p.innerHTML = "Hello, World!";
-app!.appendChild(p);
-
 const { scene, camera, renderer } = createWorld(app!);
 
-const mesh = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(mesh, material);
-scene.add(cube);
-camera.position.z = 6;
+const keyLight = new THREE.AmbientLight(0xffffff);
+const controls = new OrbitControls(camera, renderer.domElement);
 
+scene.add(keyLight);
+keyLight.position.set(40, 100, 50);
+
+const gltfLoader = new GLTFLoader();
+loadModel(gltfLoader, "/models/pacman_arcade__animation/scene.gltf", scene);
+camera.position.set(0, 100, 100);
+camera.rotateY(Math.PI);
+
+renderer.setClearColor("gray", 1);
 function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
+	controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 }
 
 animate();
+
+
+function loadModel(model: GLTFLoader, path: string, scene: THREE.Scene){
+	model.load(
+		path,
+		function (gltf) {
+			scene.add(gltf.scene);
+			gltf.animations;
+			Array<THREE.AnimationClip>;
+			gltf.scene;
+			THREE.Group;
+			gltf.scenes;
+			Array<THREE.Group>;
+			gltf.cameras;
+			Array<THREE.Camera>;
+			gltf.asset;
+			Object;
+		},
+		function (xhr) {
+			console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+		},
+		function (error) {
+			console.log("An error happened: ", error);
+		}
+	);
+}
--- src/world.ts
+++ src/world.ts
@@ -1,5 +1,9 @@
 import {Scene, PerspectiveCamera, WebGLRenderer} from "three";
 
+// const mesh = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(mesh, material);
+// scene.add(cube);
 export function createWorld(parentElement: HTMLElement): {
 	scene: THREE.Scene;
 	camera: THREE.PerspectiveCamera;
