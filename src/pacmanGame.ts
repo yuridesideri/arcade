82b21d1976dedf6fe48282eca54a3ac9d2753f0d
Yuri Desideri <yuridesideri12@gmail.com>
2023-03-28 20:23:27 -0300

    feat: pac-man ghost colision

--- src/pacmanGame.ts
+++ src/pacmanGame.ts
@@ -3,6 +3,7 @@ import { Vector2 } from "three";
 import { createGhost, createPacman } from "./objectLoader";
 import { GhostNames, MapPointDirections, PacmanMap, userControls } from "./types/pacmanGame";
 import pacmanMap, { pacmanStartingPoint } from "./constants/pacmanMap";
+import { Tween } from "@tweenjs/tween.js";
 
 export async function pacmanGame(mainRenderer: THREE.WebGLRenderer) {
 	const scene = new THREE.Scene();
@@ -14,6 +15,13 @@ export async function pacmanGame(mainRenderer: THREE.WebGLRenderer) {
 		scene,
 		pacmanStartingPoint
 	);
+	pacman.userData.direction = new THREE.Vector2(1, 0);
+	pacman.userData.speed = 0.01;
+	pacman.userData.rotation = 0;
+	pacman.userData.lives = 3;
+	pacman.userData.poweredUp = false;
+	pacman.userData.poweredUpTime = 0;
+	pacman.userData.lostLive = false;
 	const { ghost: redGhost, mixer: redGhostMixer } = await createGhost(
 		scene,
 		"/models/ghosts/ghost-red-animated.gltf",
@@ -47,13 +55,7 @@ export async function pacmanGame(mainRenderer: THREE.WebGLRenderer) {
 		side: THREE.DoubleSide,
 	});
 	const cube = new THREE.Mesh(geo, mat);
-	const z = 0.1;
 	scene.add(cube);
-	const size = 10;
-	const divisions = 32;
-	const gridHelper = new THREE.GridHelper(size, divisions);
-	gridHelper.rotateX(Math.PI / 2);
-	gridHelper.position.set(0, 0, 0.001);
 
 	const texture = new THREE.TextureLoader().load(
 		"textures/pac-man-screen/screen-texture.png"
@@ -65,6 +67,12 @@ export async function pacmanGame(mainRenderer: THREE.WebGLRenderer) {
 	scene.add(plane);
 
 	const updatePacman = PacmanGameLogic(pacman, userControls);
+	const updateBlinky = GhostLogic(redGhost, 'blinky');
+	const updatePinky = GhostLogic(pinkGhost, 'pinky');
+	const updateInky = GhostLogic(blueGhost, 'inky');
+	const updateClyde = GhostLogic(yellowGhost, 'clyde');
+
+	setInterval(() => console.log(pacman.userData), 1000)
 
 	function pacmanGameLoop() {
 		mainRenderer.render(scene, camera);
@@ -75,17 +83,21 @@ export async function pacmanGame(mainRenderer: THREE.WebGLRenderer) {
 		pinkGhostMixer?.update(0.01);
 
 		updatePacman();
+		updateBlinky();
+		updatePinky();
+		updateInky();
+		updateClyde();
+
+		PacManGhostColisionChecker(pacman, redGhost);
+		PacManGhostColisionChecker(pacman, blueGhost);
+		PacManGhostColisionChecker(pacman, yellowGhost);
+		PacManGhostColisionChecker(pacman, pinkGhost);
 	}
 
 	return pacmanGameLoop;
 }
 
 function PacmanGameLogic(pacman: THREE.Group, userControls: userControls) {
-	const pacmanControls = {
-		speed: 0.01,
-		direction: new THREE.Vector2(1, 0),
-		rotation: 0,
-	};
 
 	window.addEventListener("keydown", (e) => {
 		console.log("pressed");
@@ -102,22 +114,37 @@ function PacmanGameLogic(pacman: THREE.Group, userControls: userControls) {
 			userControls.direction = new THREE.Vector2(0, -1);
 		}
 		if (
-			userControls.direction.x === -1 * pacmanControls.direction.x ||
-			userControls.direction.y === -1 * pacmanControls.direction.y
+			userControls.direction.x === -1 * pacman.userData.direction.x ||
+			userControls.direction.y === -1 * pacman.userData.direction.y
 		) {
-			pacmanControls.direction.x = userControls.direction.x;
-			pacmanControls.direction.y = userControls.direction.y;
-			pacmanControls.rotation =
-				Math.asin(pacmanControls.direction.y) ||
-				Math.asin(pacmanControls.direction.x) + -Math.PI / 2;
+			pacman.userData.direction.x = userControls.direction.x;
+			pacman.userData.direction.y = userControls.direction.y;
+			pacman.userData.rotation =
+				Math.asin(pacman.userData.direction.y) ||
+				Math.asin(pacman.userData.direction.x) + -Math.PI / 2;
 		}
 	});
+
+	function resetPacman(){
+		pacman.position.x = pacmanStartingPoint.x;
+		pacman.position.y = pacmanStartingPoint.y;
+		pacman.userData.direction = new THREE.Vector2(1, 0);
+		pacman.userData.rotation = 0;
+		pacman.userData.speed = 0.01;
+		setTimeout(() => pacman.userData.speed = 0.01, 10) 
+		pacman.userData.lostLive = false;
+	}
 
 	function updatePacman() {
 		const possibleMapColision = mapColisionChecker(
 			new Vector2(pacman.position.x, pacman.position.y),
 			pacmanMap
 		);
+			if (pacman.userData.lostLive){
+				resetPacman();
+				userControls.direction = new THREE.Vector2(1, 0);
+				pacman.userData.lives -= 1;
+			}
 
 		if (possibleMapColision) {
 			if (
@@ -128,27 +155,27 @@ function PacmanGameLogic(pacman: THREE.Group, userControls: userControls) {
 					userControls.direction.y
 				)
 			) {
-				pacmanControls.direction = userControls.direction;
-				pacmanControls.rotation =
-					Math.asin(pacmanControls.direction.y) ||
-					Math.asin(pacmanControls.direction.x) + -Math.PI / 2;
-				pacmanControls.speed = 0.01;
+				pacman.userData.direction = userControls.direction;
+				pacman.userData.rotation =
+					Math.asin(pacman.userData.direction.y) ||
+					Math.asin(pacman.userData.direction.x) + -Math.PI / 2;
+				pacman.userData.speed = 0.01;
 			} else if (
 				(<MapPointDirections>possibleMapColision[1]).x.includes(
-					pacmanControls.direction.x
+					pacman.userData.direction.x
 				) ||
 				(<MapPointDirections>possibleMapColision[1]).y.includes(
-					pacmanControls.direction.y
+					pacman.userData.direction.y
 				)
 			) {
 			} else {
-				pacmanControls.speed = 0;
+				pacman.userData.speed = 0;
 			}
 			console.log("map colision");
 		}
-		pacman.position.x += pacmanControls.speed * pacmanControls.direction.x;
-		pacman.position.y += pacmanControls.speed * pacmanControls.direction.y;
-		pacman.rotation.y = pacmanControls.rotation;
+		pacman.position.x += pacman.userData.speed * pacman.userData.direction.x;
+		pacman.position.y += pacman.userData.speed * pacman.userData.direction.y;
+		pacman.rotation.y = pacman.userData.rotation;
 		if (pacman.position.x > 5) {
 			pacman.position.x = -5;
 		}
@@ -183,18 +210,77 @@ function mapColisionChecker(entityPosition: THREE.Vector2, map: PacmanMap) {
 	return mapColisionPoint;
 }
 
+function normalColisionChecker(entityPosition1: THREE.Vector2, entitiyPosition2: THREE.Vector2, radius: number){
+	const entityPosition1Rounded = new THREE.Vector2(
+		Math.round(entityPosition1.x * 100) / 100,
+		Math.round(entityPosition1.y * 100) / 100
+	);
+	const entityPosition2Rounded = new THREE.Vector2(
+		Math.round(entitiyPosition2.x * 100) / 100,
+		Math.round(entitiyPosition2.y * 100) / 100
+	);
+	const distance = entityPosition1Rounded.distanceTo(entityPosition2Rounded);
+	if (distance < radius){
+		return true;
+	}
+	return false;
+}
+
+function PacManGhostColisionChecker(pacman: THREE.Group, ghost: THREE.Group){
+	const radius = 0.34;
+	const pacmanPosition = new THREE.Vector2(pacman.position.x, pacman.position.y);
+	const ghostPosition = new THREE.Vector2(ghost.position.x, ghost.position.y);
+	if (normalColisionChecker(pacmanPosition, ghostPosition, radius)){
+		pacman.userData.lostLive = true;
+	}
+}
+
 function GhostLogic(ghost: THREE.Group, type: GhostNames){
-	let timeInterval;
+	let ghostFree = false;
+	let ghostDirection = new THREE.Vector2(1, 0);
+	let ghostSpeed = 0.01;
+	let timeInterval : number;
 	if (type === "blinky"){
-		timeInterval = 5000;
+		timeInterval = 5 * 1000;
 	} else if (type === "pinky"){
-		timeInterval = 10000;
+		timeInterval = 20 * 1000;
 	}
 	else if (type === "inky"){
-		timeInterval = 15000;
+		timeInterval = 40 * 1000;
 	}
 	else if (type === "clyde"){
-		timeInterval = 20000;
+		timeInterval = 60 * 1000;
 	}
+	const moveGhostToCenter = new Tween(ghost.position).to({x: 0, y: 0.35}, 2000).delay(timeInterval!)
+	const moveGhostToTheBoard = new Tween(ghost.position).to({x: 0, y: 1.25}, 2000).onComplete(() => ghostFree = true);
+	moveGhostToCenter.chain(moveGhostToTheBoard).start();
 	
+	function updateGhost(){
+		if (ghostFree){
+			const possibleMapColision = mapColisionChecker(new Vector2(ghost.position.x, ghost.position.y), pacmanMap);
+			if (possibleMapColision){
+				const chooseXorY = Math.floor(Math.random() * 2);
+				const possibleDirections = <MapPointDirections>possibleMapColision[1];
+				if (chooseXorY === 0){
+					const possibleValues = Math.floor(possibleDirections.x.length * Math.random())
+					ghostDirection = new THREE.Vector2(possibleDirections.x[possibleValues], 0);
+				}
+				else if (chooseXorY === 1){
+					const possibleValues = Math.floor(possibleDirections.y.length * Math.random())
+					ghostDirection = new THREE.Vector2(0, possibleDirections.y[possibleValues]);
+				}
+
+			}
+			ghost.position.x += ghostDirection.x * ghostSpeed;
+			ghost.position.y += ghostDirection.y * ghostSpeed;
+			if (ghost.position.x > 5){
+				ghost.position.x = -5;
+			}
+			if (ghost.position.x < -5){
+				ghost.position.x = 5;
+			}
+		}
+
+	}
+	return updateGhost;
 }
